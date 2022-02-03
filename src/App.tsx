0a2d52f1397e50b77d262e797d5ead8aec03bb20
kokoball <kbp4154@naver.com>
2022-02-03 22:51:57 +0900

    CSS 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { FC, ChangeEvent, useState } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadline] = useState<number>(0);
+  const [todo, setTodoList] = useState([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {};
+
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="inputContainer">
+          <input
+            type="text"
+            placeholder="Task..."
+            name="task"
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            placeholder="Deadline (in Days)..."
+            name="deadline"
+            onChange={handleChange}
+          />
+        </div>
+        <button>Add Task</button>
+      </div>
+      <div className="todoList"></div>
+    </div>
+  );
+};
 
 export default App;
